e12b545672bef41486c9c187de2d709dcb4628bb
mahmo9ud <mohnud7890@gmail.com>
2026-02-07 21:46:57 +0200

    updata

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,7 +5,7 @@ import axios, { AxiosError } from "axios";
 // import { toast } from "react-hot-toast";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
+console.log(BASE_URL);
 export const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
